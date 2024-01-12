d3c6500c634b837deee2819ccf99fd45263871aa
Bas Broere <bas@basbroere.nl>
2024-01-12 20:32:32 +0100

    Add p7

---
+++ src/challenges/p7.ts
@@ -0,0 +1,23 @@
+export {};
+
+function checkPrime(n: number): boolean {
+    const limit:number = Math.ceil(n / 2);
+    for (let i = 2; i < limit; i++) {
+        if (n % i == 0) {
+            return false;
+        }
+    }
+    return true;
+}
+
+function run(): void {
+    let latestPrime: number = 2;
+    let latestPrimeCnt: number = 1;
+    for (let i: number = 3; latestPrimeCnt != 10001; i += 2) {
+        if (checkPrime(i)) {
+            latestPrime = i;
+            latestPrimeCnt += 1;
+        }
+    }
+    console.log(latestPrime);
+}
