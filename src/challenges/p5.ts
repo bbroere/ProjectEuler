0be94f106ca38f9d9672e08a939aca0432b6374a
Bas Broere <bas@basbroere.nl>
2024-01-12 20:09:19 +0100

    Fix pt 5

--- src/challenges/p5.ts
+++ src/challenges/p5.ts
@@ -1,23 +1,23 @@
 export {};
 
-function run(): void {
-    // Let's do this smart
-    // Start from 20 and check of all numbers upto 20 that it is divisible by
-    // Do the same for 19 and so on until you have a complete list of all factors
-    const realisedFactors: number[] = [];
-    let res: number = 1;
-    let c: number = 20;
-    while (c > 0) {
-        if (!realisedFactors.includes(c)) {
-            res *= c;
-            realisedFactors.push(c);
-            for (let i = 0; i < c; i++) {
-                if (c % i == 0 && !realisedFactors.includes(i)) {
-                    realisedFactors.push(i);
-                }
-            }
+function lcm(a: number, b: number): number {
+    let a_r: number = a;
+    let b_r: number = b;
+    while (a_r != b_r) {
+        if (a_r < b_r) {
+            a_r += a;
+        } else {
+            b_r += b;
         }
-        c--;
+    }
+    return a_r;
+}
+
+function run(): void {
+    // This is simply the lcm of all numbers
+    let res: number = 1;
+    for (let i = 2; i < 21; i++) {
+        res = lcm(res, i);
     }
     console.log(res);
 }
