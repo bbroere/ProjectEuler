d2419f8c1f337df947e4df8877255f85ea9bf185
Bas Broere <bas@basbroere.nl>
2024-01-12 18:54:59 +0100

    Add p4

---
+++ src/challenges/p4.ts
@@ -0,0 +1,18 @@
+export {};
+
+function checkPalindrome(n: number): boolean {
+    return `${n}` === `${n}`.split("").reverse().join("");
+}
+
+function run(): void {
+    // Not the best way to do this but on this scale it's fine
+    let res: number = 0;
+    for (let i: number = 100; i < 1000; i++) {
+        for (let j: number = 100; j < 1000; j++) {
+            if (checkPalindrome(i * j) && i * j > res) {
+                res = i * j;
+            }
+        }
+    }
+    console.log(res);
+}
