3a9fbc6c1a12875a971dee0abf5f98ddd0c9f859
Bas Broere <bas@basbroere.nl>
2024-01-12 18:45:57 +0100

    Add p3

---
+++ src/challenges/p3.ts
@@ -0,0 +1,16 @@
+export {};
+
+function run(): void {
+    // let's start simple, when we find a factor, divide it out and break if need be
+    let original: number = 600851475143;
+    let c: number = 2;
+    const factors: number[] = [];
+    while (original > c) {
+        if (original % c == 0) {
+            factors.push(c);
+            original = original / c;
+        }
+        c++;
+    }
+    console.log(Math.max(...factors));
+}
