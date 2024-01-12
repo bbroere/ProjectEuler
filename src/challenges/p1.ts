b8bf79335a1fafcd17b3f676b2b9f7aa92be634a
Bas Broere <bas@basbroere.nl>
2024-01-12 16:18:18 +0100

    Add p1 and p2

--- src/challenges/p1.ts
+++ src/challenges/p1.ts
@@ -1,5 +1,11 @@
+export {}
+
 function run(): void {
-    for (let i = 0; i < 100; i++) {
-        console.log(i)
+    let sum: number = 0;
+    for (let i: number = 0; i < 1000; i++) {
+        if (i % 3 == 0 || i % 5 == 0) {
+            sum += i
+        }
     }
+    console.log(sum)
 }
---
+++ src/challenges/p2.ts
@@ -0,0 +1,17 @@
+export {}
+
+function run(): void {
+    // possible improvement: fib sequence is (odd - odd - even) repeated
+    let f1: number = 1;
+    let f2: number = 1;
+    let sum: number = 0;
+    while (f1 + f2 <= 4000000) {
+        const next = f1 + f2
+        if (next % 2 == 0) {
+            sum += next
+        }
+        f1 = f2
+        f2 = next
+    }
+    console.log(sum)
+}
