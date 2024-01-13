25e239cf08e17329b268287ecee1cd1e321065af
Bas Broere <bas@basbroere.nl>
2024-01-13 12:47:35 +0100

    Add p20

---
+++ src/challenges/p20.ts
@@ -0,0 +1,15 @@
+export {};
+
+function run(): void {
+    let res = BigInt(1);
+    for (let i = 2; i <= 100; i++) {
+        res *= BigInt(i);
+    }
+    console.log(
+        res
+            .toString()
+            .split("")
+            .map(Number)
+            .reduce((c, n) => c + n, 0)
+    );
+}
