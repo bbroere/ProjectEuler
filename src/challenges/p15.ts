cb9dc8e0bf7aa9286af1cf2211ce3e6127d8fa7a
Bas Broere <bas@basbroere.nl>
2024-01-13 11:11:12 +0100

    Add p15

---
+++ src/challenges/p15.ts
@@ -0,0 +1,17 @@
+export {};
+
+function run(): void {
+    // This is just the binomial coefficient ((2 * 20) 20)
+    // This equals:
+    // (40 * 39 * 38 * ... * 21) / (20 * 19 * ... * 1)
+    let n = 1;
+    let d = 1;
+    for (let i = 1; i <= 40; i++) {
+        if (i > 20) {
+            n *= i;
+        } else {
+            d *= i;
+        }
+    }
+    console.log(n / d);
+}
