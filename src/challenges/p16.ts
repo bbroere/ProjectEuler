fb0aa5f6742f5debd71bbfabb6f0974691b124d3
Bas Broere <bas@basbroere.nl>
2024-01-13 11:13:05 +0100

    Add p16

---
+++ src/challenges/p16.ts
@@ -0,0 +1,11 @@
+export {};
+
+function run(): void {
+    console.log(
+        BigInt(Math.pow(2, 1000))
+            .toString()
+            .split("")
+            .map(Number)
+            .reduce((c, n) => c + n, 0)
+    );
+}
