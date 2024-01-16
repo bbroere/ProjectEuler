4631bc9960b22d01e3863b6d7fb3f6fdc9aa4624
Bas Broere <bas@basbroere.nl>
2024-01-16 14:01:42 +0100

    Add p30

---
+++ src/challenges/p30.ts
@@ -0,0 +1,22 @@
+import {sumWithCondition} from "../utilities/sequences";
+
+function upperBound(p: number): number {
+    // This problem has no solution for integers with n digits if n*9^p < 10^(n-1)
+    let n: number = 1;
+    while (Math.pow(10, n - 1) / n < Math.pow(9, p)) {
+        n++;
+    }
+    return Math.ceil(Math.pow(10, n) / n);
+}
+
+export function run(): number {
+    const power: number = 5;
+    const ub: number = upperBound(power);
+    const res: number[] = [];
+    for (let n: number = 2; n < ub; n++) {
+        if (`${n}`.split("").map(Number).reduce((c: number, n: number) => c + Math.pow(n, power), 0) == n) {
+            res.push(n);
+        }
+    }
+    return sumWithCondition(res);
+}
