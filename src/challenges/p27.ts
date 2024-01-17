173eb65718fd3ff54e5d763461d4acff5e011673
Bas Broere <bas@basbroere.nl>
2024-01-17 10:12:39 +0100

    Add p27

---
+++ src/challenges/p27.ts
@@ -0,0 +1,35 @@
+import {primesWithUpperBound} from "../utilities/primes";
+
+function lengthStartingPrimeSequence(primes: number[], a: number, b: number): number {
+    let n: number = 0;
+    while (primes.includes(n * n + a * n + b)) {
+        n++;
+    }
+    // If this happens the list of primes was too small
+    if (n * n + a * n + b > primes.slice(-1)[0]) {
+        return -1;
+    }
+    return n;
+}
+
+export function run(): number {
+    const lb: number = -999;
+    const ub: number = 999;
+    let bestNofPrimes: [number, number, number] = [-1, 0, 0];
+    // Low-ball, we up it if need be, but inclusion is faster the smaller the array is
+    let primesBound: number = 100;
+    let primes: number[] = primesWithUpperBound(primesBound);
+    for (let a: number = lb; a <= ub; a++) {
+        for (let b: number = lb; b <= ub; b++) {
+            const l: number = lengthStartingPrimeSequence(primes, a, b);
+            if (l == -1) {
+                primesBound *= 2;
+                primes = primesWithUpperBound(primesBound);
+                b--;
+                console.log(`Doubled primes bound to ${primesBound}`);
+            }
+            if (l > bestNofPrimes[0]) bestNofPrimes = [l, a, b];
+        }
+    }
+    return bestNofPrimes[1] * bestNofPrimes[2];
+}
