a6bf7f7d79d017c5a725edc8ffa3cc6154fc60ef
Bas Broere <bas@basbroere.nl>
2024-07-25 20:24:00 +0200

    Add p48

---
+++ src/challenges/p48.ts
@@ -0,0 +1,13 @@
+export function run(): number {
+    const max: number = 1000;
+    const nofDigits: number = 10;
+    let res: number = 0;
+    for (let i: number = 1; i < max; i++) {
+        let r: number = 1;
+        for (let j: number = 1; j <= i; j++) {
+            r = (r * i) % 10 ** nofDigits;
+        }
+        res = (res + r) % 10 ** nofDigits;
+    }
+    return res;
+}
