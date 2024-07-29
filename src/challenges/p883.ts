5cd26b17ec390a57836ba0a5cc5f4cdd59106cb4
Bas Broere <bas@basbroere.nl>
2024-07-29 10:13:55 +0200

    More LaTeX stuff

--- src/challenges/p883.ts
+++
@@ -1,6 +0,0 @@
-export function run(): number {
-    const max: number = 10;
-    const N1: number[] = Array.from({length: max}, (_, i: number) => i + 1);
-    const NS1: number[] = N1.map((n: number) => Math.sqrt(3) * n).filter((n: number): boolean => n <= max);
-    return 0;
-}
