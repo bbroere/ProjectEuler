43d39e5cf4bb275823067c747601243fed3db422
Bas Broere <bas@basbroere.nl>
2024-08-02 12:29:43 +0200

    Add time function

--- src/challenges/p1.ts
+++ src/challenges/p1.ts
@@ -1,5 +1,6 @@
 import {numbersWithMaxSize, sum} from "../utilities/sequences";
 
+// Average runtime ~0.04 ms
 export function run(): number {
     return sum(numbersWithMaxSize(999), (i: number) => i % 3 == 0 || i % 5 == 0);
 }
