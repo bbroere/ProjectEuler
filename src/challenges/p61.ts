1483a21b098ecf81cff24456ef60a891bb29da5f
Bas Broere <bas@basbroere.nl>
2024-08-07 16:22:32 +0200

    Cleanup

--- src/challenges/p61.ts
+++ src/challenges/p61.ts
@@ -12,35 +12,34 @@ export function run(): number {
                     .map((n: number): [number, number] => [s + 3, n])
         );
 
+    function extendCurrentMatch(currentSet: [number, number][], options: [number, number][]): [number, number][] | undefined {
+        if (currentSet.length === 6) {
+            if (currentSet[0][1].toString().slice(0, 2) === currentSet[5][1].toString().slice(2))
+                return currentSet;
+            return undefined;
+        } else {
+            const lastNumber: number = currentSet[currentSet.length - 1][1];
+            const lastTwoDigits: string = lastNumber.toString().slice(2);
+            const nextOptions: [number, number][] = options.filter(([s, n]) => n.toString().slice(0, 2) === lastTwoDigits);
+            for (let i = 0; i < nextOptions.length; i++) {
+                const nextOption: [number, number] = nextOptions[i];
+                const newSet: [number, number][] = [...currentSet, nextOption];
+                const result: [number, number][] | undefined = extendCurrentMatch(
+                    newSet,
+                    options.filter(([s, n]) => !newSet.map(t => t[0]).includes(s) && !newSet.map(t => t[1]).includes(n))
+                );
+                if (result !== undefined)
+                    return result;
+            }
+            return undefined;
+        }
+    }
+
     for (let i = 0; i < allTriangleNumbers.length; i++) {
         const match: [number, number][] | undefined = extendCurrentMatch([[3, allTriangleNumbers[i]]], allSPolygonals);
         if (match !== undefined)
             return match.reduce((acc, [s, n]) => acc + n, 0);
     }
 
-
     return undefined;
-}
-
-function extendCurrentMatch(currentSet: [number, number][], options: [number, number][]): [number, number][] | undefined {
-    if (currentSet.length === 6) {
-        if (currentSet[0][1].toString().slice(0, 2) === currentSet[5][1].toString().slice(2))
-            return currentSet;
-        return undefined;
-    } else {
-        const lastNumber: number = currentSet[currentSet.length - 1][1];
-        const lastTwoDigits: string = lastNumber.toString().slice(2);
-        const nextOptions: [number, number][] = options.filter(([s, n]) => n.toString().slice(0, 2) === lastTwoDigits);
-        for (let i = 0; i < nextOptions.length; i++) {
-            const nextOption: [number, number] = nextOptions[i];
-            const newSet: [number, number][] = [...currentSet, nextOption];
-            const result: [number, number][] | undefined = extendCurrentMatch(
-                newSet,
-                options.filter(([s, n]) => !newSet.map(t => t[0]).includes(s) && !newSet.map(t => t[1]).includes(n))
-            );
-            if (result !== undefined)
-                return result;
-        }
-        return undefined;
-    }
 }
