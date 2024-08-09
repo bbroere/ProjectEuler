3e94e16598f802b7c594ed3ac84664e4ab1213a2
Bas Broere <bas@basbroere.nl>
2024-08-09 07:59:55 +0200

    Test with new problem generation

--- src/challenges/p1.ts
+++ src/challenges/p1.ts
@@ -1,6 +1,12 @@
 import {numbersWithMaxSize, sum} from "../utilities/sequences";
 
-// Average runtime ~0.04 ms
+/**
+ * <p>If we list all the natural numbers below $10$ that are multiples of $3$ or $5$, we get $3, 5, 6$ and $9$. The sum of these multiples is $23$.</p>
+ * <p>Find the sum of all the multiples of $3$ or $5$ below $1000$.</p>
+ *
+ * <p><a href='https://projecteuler.net/problem=1'>Source</a></p>
+ * <p><i><b>Average runtime ~??? ms</b></i></p>
+ */
 export function run(): number {
     return sum(numbersWithMaxSize(999), (i: number) => i % 3 == 0 || i % 5 == 0);
 }
--- src/gen.ts
+++ src/gen.ts
@@ -1,14 +1,29 @@
 import {writeFileSync} from "node:fs";
 import {readdirSync} from "fs";
+import * as https from "node:https";
+import {IncomingMessage} from "node:http";
 
 function genProblem(n: string): void {
     if (readdirSync('challenges').includes(`p${n}.ts`)) throw new Error(`p${n}.ts already exists! Aborting...`);
-    const content: string =
-        '// Average runtime ~??? ms\n' +
-        'export function run(): number {\n' +
-        '    return undefined;\n' +
-        '}\n';
-    writeFileSync(`challenges/p${n}.ts`, content, {encoding: 'utf8'});
+    https.get(`https://projecteuler.net/minimal=${n}`).on('response', (res: IncomingMessage) =>
+        res.on('data', problem => {
+            const sourcesExtended = problem.toString().replaceAll('src="', 'src="https://projecteuler.net/').replace(/\n+$/, "");
+            const problemJavaDoc =
+                [
+                    "/**",
+                    ...sourcesExtended.split("\n").map((line: string) => ` * ${line}`),
+                    " *",
+                    ` * <p><a href='https://projecteuler.net/problem=${n}'>Source</a></p>`,
+                    " * <p><i><b>Average runtime ~??? ms</b></i></p>",
+                    " */\n"
+                ].join("\n");
+            const content: string = problemJavaDoc +
+                'export function run(): number {\n' +
+                '    return undefined;\n' +
+                '}\n';
+            writeFileSync(`challenges/p${n}.ts`, content, {encoding: 'utf8'});
+        })
+    );
 }
 
 genProblem(process.argv[2]);
